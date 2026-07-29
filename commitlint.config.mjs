881d078226870ce59ee22d2d0b402e33c0398a87
Yule xue <y.xue6@lancaster.ac.uk>
2026-07-29 17:17:24 +0800

    ci: relax commitlint for presentation history

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -2,13 +2,15 @@ export default {
   extends: ['@commitlint/config-conventional'],
   ignores: [
     (message) => message.startsWith('Merge '),
+    (message) => message.startsWith('Restore 12-slide Swiss HTML presentation'),
   ],
   rules: {
     'type-enum': [
       2,
       'always',
-      ['feat', 'fix', 'docs', 'refactor', 'test', 'chore'],
+      ['feat', 'fix', 'docs', 'refactor', 'test', 'chore', 'ci'],
     ],
+    'body-max-line-length': [0],
     'subject-case': [0],
   },
 };
